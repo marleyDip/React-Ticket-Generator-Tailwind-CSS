030f2d150c10f68d33acf84b58dffa82048babdb
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-17 03:19:41 +0600

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
       <div className="absolute inset-0">
         <div className="absolute top-20 right-20 w-96 h-96 bg-gradient-to-r from-orange-500/10 to-purple-500/10 rounded-full blur-3xl"></div>
 
-        <div className=""></div>
+        <div className="absolute bottom-20 left-20 w-80 h-80 bg-gradient-to-r from-purple-500/10 to-pink-500/10 rounded-full blur-3xl"></div>
 
-        <div className=""></div>
+        <div className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-gradient-to-r from-blue-500/50 to-purple-500/50 rounded-full blur-3xl"></div>
       </div>
       {/* background */}
       {/* grid pattern overlay */}
